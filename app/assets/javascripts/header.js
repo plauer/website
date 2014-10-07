820ac8036dd5f581168579dd03606fc9a4d80439
plauer <plauer411@gmail.com>
2014-10-06 23:08:54 -0700

    snapshot. still whitespace issue under fixed header

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,11 +2,13 @@ $(document).ready( function() {
  
   var showNavMenu = function(event) {
     $('header .nav-menu').slideDown(100);
+    $('header .dropdown .glyphicon').css('color', 'lightblue')
   }
 
   var hideNavMenu = function(event) {
     setTimeout( function() {
       $('header .nav-menu').slideUp(100);
+      $('header .dropdown .glyphicon').css('color', 'black')
     }, 1000);
   }
 
