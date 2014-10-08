c6d5e421b900f7de0e076e1f7ab174a40f54a716
plauer <plauer411@gmail.com>
2014-10-07 19:34:12 -0700

    changes some welcome page content

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,18 +1,9 @@
 $(document).ready( function() {
  var img = $('.welcome-image')
- // $(window).scroll( function() {
- //  if ($(window).scrollTop()<10) {
- //    img.stop(true, true).hide().fadeIn(5000);
- //  } else {
- //    img.stop(true, true).show().fadeOut(5000);
- //  }
- // })
 
-
- $(window).scroll(function() {
+  $(window).scroll(function() {
     $(img).css({
-    'opacity' : 1-(($(this).scrollTop())/475)
+    'opacity' : 1-(($(this).scrollTop())/600)
     });          
-});
-
+  });
 })
